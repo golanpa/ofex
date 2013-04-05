77da09f974158a8df115bae484012671cf68758b
Golan Parashi <golanp@wix.com>
2013-04-05 15:35:02 +0300

    some refactoring

--- of_learning_switch_spanning_tree.py
+++ of_learning_switch_spanning_tree.py
@@ -14,16 +14,6 @@ import utils
 log = core.getLogger()
 
 
-class HostInfo(object):
-    def __init__(self, eth_addr, in_port=-1):
-        self.eth_addr = eth_addr
-        self.in_port = in_port
-        self.rule_installed = False
-
-    def __str__(self):
-        return 'mac={}, in_port={}, installed={}'.format(self.eth_addr, self.in_port, self.rule_installed)
-
-
 class Tutorial(object):
     """
     A Tutorial object is created for each switch that connects.
@@ -31,26 +21,10 @@ class Tutorial(object):
     """
     def __init__(self, connection):
         self.connection = connection
-        self.mac_to_host = dict()
-
-        # self._install_lldp_to_controller_flow()
+        self.mac_to_in_port = dict()
 
         # This binds our PacketIn event listener
         connection.addListeners(self)
-
-    def _install_lldp_to_controller_flow(self):
-        from pox.lib.packet.ethernet import LLDP_MULTICAST
-        """ Installing rule in switch. Rule is from any source to specific destination via dst_port """
-
-        log.debug('Installing LLDP flow: dpid={}, match: type={}, dst={} output to controller via port {}'
-                  .format(self.connection.dpid, pkt.ethernet.LLDP_TYPE, LLDP_MULTICAST, of.OFPP_CONTROLLER))
-
-        msg = of.ofp_flow_mod()
-        msg.match.dl_type = pkt.ethernet.LLDP_TYPE
-        msg.match.dl_dst = LLDP_MULTICAST
-        msg.actions.append(of.ofp_action_output(port=of.OFPP_CONTROLLER))
-
-        self.connection.send(msg)
 
     def _handle_PacketIn(self, event):
         """
@@ -100,63 +74,57 @@ class Tutorial(object):
 
     def act_like_switch(self, packet, packet_in):
         """
-        Implement switch-like behavior -- if destination is know - send only to the learnt port, otherwise, send all
-        packets to all ports besides the input port.
+        Implement switch-like behavior -- if destination is know, send only to the learnt port, otherwise, flood (send
+        all packets to all ports besides the input port).
         """
 
         dpid = self.connection.dpid
 
         log.debug('act_like_switch: dpid={}, type={}, {}.{} -> {}'
-                  .format(dpid, packet.type, packet.src, packet_in.in_port, packet.dst))
+        .format(dpid, packet.type, packet.src, packet_in.in_port, packet.dst))
 
         # check if we already know this src
-        host_info = self.mac_to_host.get(packet.src, None)
-        if host_info:
+        known_in_port = self.mac_to_in_port.get(packet.src, None)
+        if known_in_port:
             # check whether src incoming port was changed
-            if host_info.in_port != packet_in.in_port:
-                log.info('src in_port was changed: dpid={}, info={}, new in_port={}'
-                         .format(dpid, host_info, packet_in.in_port))
+            if known_in_port != packet_in.in_port:
+                log.info('src in_port was changed: dpid={}, src={} known in_port={}, new in_port={}'
+                .format(dpid, packet.src, known_in_port, packet_in.in_port))
 
-                if host_info.rule_installed:
-                    self._uninstall_flow(dpid, packet, host_info.in_port)
-                    host_info.rule_installed = False
+                self._uninstall_flows(dpid, packet, known_in_port)
 
                 log.debug('Learning: dpid={}, {} via port {}'.format(dpid, packet.src, packet_in.in_port))
-                host_info.in_port = packet_in.in_port
+                self.mac_to_in_port[packet.src] = packet_in.in_port
         else:
             # new src.in_port - learn it
             log.debug('Learning: dpid={}, {} via port {}'.format(dpid, packet.src, packet_in.in_port))
-            self.mac_to_host[packet.src] = HostInfo(packet.src, packet_in.in_port)
+            self.mac_to_in_port[packet.src] = packet_in.in_port
 
         # check if we know in which port the destination is connected to
-        dst_host_info = self.mac_to_host.get(packet.dst, None)
-        if dst_host_info:
+        known_in_port = self.mac_to_in_port.get(packet.dst, None)
+        if known_in_port:
             # install new rule: dst via in_port
-            self._install_flow(dpid, packet, packet_in, dst_port=dst_host_info.in_port)
-            dst_host_info.rule_installed = True
-
-            # log.info('Sending: dpid={}, type={}, {}.{} -> {}.{}'
-            #          .format(dpid, packet.type, packet.src, packet_in.in_port, packet.dst, dst_host_info.in_port))
-            # self.send_packet(packet_in.buffer_id, packet_in.data, dst_host_info.in_port, packet_in.in_port)
+            self._install_flow(dpid, packet, packet_in, dst_port=known_in_port)
         else:
             # we do not know in which port destination is connected if at all
             log.debug('Broadcasting dpid={}, type={}, {}.{} -> {}.{}'
-                      .format(dpid, packet.type, packet.src, packet_in.in_port, packet.dst, of.OFPP_FLOOD))
+            .format(dpid, packet.type, packet.src, packet_in.in_port, packet.dst, of.OFPP_FLOOD))
             self.send_packet(packet_in.buffer_id, packet_in.data, of.OFPP_FLOOD, packet_in.in_port)
 
         log.debug('act_like_switch: finished: dpid={}'.format(dpid))
 
     def _install_flow(self, dpid, packet, packet_in, dst_port):
-        """ Installing rule in switch. Rule is from any source to specific destination via dst_port """
+        """ Installing rule in switch. Rule is from any source to specific destination and src_port via dst_port """
 
         log.debug('Installing flow: dpid={}, match={{ dst:{}, in_port:{} }} output via port {}'
-                  .format(dpid, packet.dst, packet_in.in_port, dst_port))
+        .format(dpid, packet.dst, packet_in.in_port, dst_port))
 
         msg = of.ofp_flow_mod()
         msg.match.dl_dst = packet.dst
         msg.match.in_port = packet_in.in_port
         msg.actions.append(of.ofp_action_output(port=dst_port))
 
+        # also send the packet...
         if packet_in.buffer_id != of.NO_BUFFER and packet_in.buffer_id is not None:
             # We got a buffer ID from the switch; use that
             msg.buffer_id = packet_in.buffer_id
@@ -169,11 +137,11 @@ class Tutorial(object):
 
         log.debug('Sending: dpid={}, {}.{} -> {}.{}'.format(dpid, packet.src, packet_in.in_port, packet.dst, dst_port))
 
-    def _uninstall_flow(self, dpid, packet, old_port):
-        """ Un-installing previous rule. """
+    def _uninstall_flows(self, dpid, packet, old_port):
+        """ Un-installing all rules to specific destination. """
 
         log.debug('Un-installing flow: dpid={}, {} -> {} output via port {}'
-                  .format(dpid, "ff:ff:ff:ff:ff:ff", packet.src, old_port))
+        .format(dpid, "ff:ff:ff:ff:ff:ff", packet.src, old_port))
 
         msg = of.ofp_flow_mod(command=of.OFPFC_DELETE)
         msg.match.dl_dst = packet.src
@@ -184,6 +152,8 @@ class Tutorial(object):
 from collections import namedtuple
 from random import shuffle
 from utils import Timer
+from pox.lib.revent import *
+import time
 
 
 class LLDPSender(object):
@@ -204,27 +174,28 @@ class LLDPSender(object):
           other LLDP agents might.  Can't be 0 (this means revoke).
         """
         # Packets remaining to be sent in this cycle
-        self._this_cycle = []
+        self._this_cycle = list()
 
         # Packets we've already sent in this cycle
-        self._next_cycle = []
+        self._next_cycle = list()
 
         self._timer = None
         self._send_cycle_time = send_cycle_time
 
+        # register for switch events
         core.listen_to_dependencies(self)
 
     def _handle_openflow_PortStatus(self, event):
         """ Track changes to switch ports """
-        if event.added:
+        if event.added or (event.modified and event.ofp.desc.config == 0):
             self.add_port(event.dpid, event.port, event.ofp.desc.hw_addr)
-        elif event.deleted:
+        elif event.deleted or (event.modified and event.ofp.desc.config == 1):
             self.del_port(event.dpid, event.port)
 
     def _handle_openflow_ConnectionUp(self, event):
         self.del_switch(event.dpid, set_timer=False)
 
-        ports = [(p.port_no, p.hw_addr) for p in event.ofp.ports]
+        ports = [(port.port_no, port.hw_addr) for port in event.ofp.ports]
 
         for port_num, port_addr in ports:
             self.add_port(event.dpid, port_num, port_addr, set_timer=False)
@@ -259,7 +230,7 @@ class LLDPSender(object):
 
         self.del_port(dpid, port_num, set_timer=False)
 
-        lldpPacket = self.create_discovery_packet(dpid, port_num, port_addr)
+        lldpPacket = self.create_lldp_packet(dpid, port_num, port_addr)
         self._next_cycle.append(LLDPSender.SendItem(dpid, port_num, lldpPacket))
 
         if set_timer:
@@ -267,7 +238,7 @@ class LLDPSender(object):
 
     def _set_timer(self):
         if self._timer:
-            self._timer.cancel()
+            self._timer.stop()
 
         self._timer = None
         num_packets = len(self._this_cycle) + len(self._next_cycle)
@@ -289,35 +260,30 @@ class LLDPSender(object):
             self._next_cycle = []
             shuffle(self._this_cycle)
 
-        item = self._this_cycle.pop(0)
-        self._next_cycle.append(item)
-        core.openflow.sendToDPID(item.dpid, item.packet)
+        if len(self._this_cycle) > 0:
+            item = self._this_cycle.pop(0)
+            self._next_cycle.append(item)
+            core.openflow.sendToDPID(item.dpid, item.packet)
 
-    def create_discovery_packet(self, dpid, port_num, port_addr):
+    def create_lldp_packet(self, dpid, port_num, port_addr):
         """ Build discovery packet """
 
-        # chassis_id = pkt.chassis_id(subtype=pkt.chassis_id.SUB_LOCAL)
-        # chassis_id.id = bytes('dpid:' + hex(long(dpid))[2:-1])
-        chassis_id = pkt.chassis_id(subtype=pkt.chassis_id.LLDP_MULTICAST, id=str(dpid))
+        chassis_id = pkt.chassis_id(subtype=pkt.chassis_id.SUB_CHASSIS, id=str(dpid))
 
         port_id = pkt.port_id(subtype=pkt.port_id.SUB_PORT, id=str(port_num))
 
         ttl = pkt.ttl(ttl=1)
 
-        # sysdesc = pkt.system_description()
-        # sysdesc.payload = bytes('dpid:' + hex(long(dpid))[2:-1])
-
-        discovery_packet = pkt.lldp()
-        discovery_packet.tlvs.append(chassis_id)
-        discovery_packet.tlvs.append(port_id)
-        discovery_packet.tlvs.append(ttl)
-        # discovery_packet.tlvs.append(sysdesc)
-        discovery_packet.tlvs.append(pkt.end_tlv())
+        lldp_packet = pkt.lldp()
+        lldp_packet.tlvs.append(chassis_id)
+        lldp_packet.tlvs.append(port_id)
+        lldp_packet.tlvs.append(ttl)
+        lldp_packet.tlvs.append(pkt.end_tlv())
 
         eth = pkt.ethernet(type=pkt.ethernet.LLDP_TYPE)
         eth.src = port_addr
         eth.dst = pkt.ETHERNET.LLDP_MULTICAST
-        eth.payload = discovery_packet
+        eth.payload = lldp_packet
 
         po = of.ofp_packet_out(action=of.ofp_action_output(port=port_num))
         po.data = eth.pack()
@@ -327,25 +293,43 @@ class LLDPSender(object):
 class Discovery(object):
     __metaclass__ = utils.SingletonType
 
+    _flow_priority = 65000     # Priority of LLDP-catching flow (if any)
+    _link_timeout = 6         # How long until we consider a link dead
+    _timeout_check_period = 3  # How often to check for timeouts
+
+    Link = namedtuple("Link", ("dpid1", "port1", "dpid2", "port2"))
+
     def __init__(self):
-        self.nodes = dict()
+        self._adjacency = dict()  # From Link to time.time() stamp
+        self._sender = LLDPSender()
 
         # Listen with a high priority so we get PacketIns as soon as possible
         core.listen_to_dependencies(self, listen_args={'openflow': {'priority': 0xffffffff}})
 
-    def _handle_ConnectionUp(self, event):
-        """ Will be called when a switch is added. """
-        self.nodes[event.dpid] = event
+        # TODO: removed for debug - need to uncomment next line
+        # Timer(self._timeout_check_period, self._expire_links, recurring=True)
 
     def _handle_openflow_ConnectionUp(self, event):
-        log.debug('_handle_openflow_ConnectionUp: dpid={}, {}'.format(event.dpid, str(event)))
-        pass
+        """ Will be called when a switch is added. """
+        log.debug('_handle_openflow_ConnectionUp-> Installing flow: route LLDP messages to controller: dpid={}, {}'
+                  .format(event.dpid, str(event)))
 
-    def handle_ConnectionDown(self, event):
+        # Make sure we get LLDP traffic
+        match = of.ofp_match(dl_type=pkt.ethernet.LLDP_TYPE, dl_dst=pkt.ETHERNET.LLDP_MULTICAST)
+        msg = of.ofp_flow_mod()
+        msg.priority = Discovery._flow_priority
+        msg.match = match
+        msg.actions.append(of.ofp_action_output(port=of.OFPP_CONTROLLER))
+
+        event.connection.send(msg)
+
+    def _handle_openflow_ConnectionDown(self, event):
         """ Will be called when a switch goes down. """
-        del self.nodes[event.dpid]
+        # Delete all links on this switch
+        self._delete_links([link for link in self._adjacency if link.dpid1 == event.dpid or link.dpid2 == event.dpid])
 
-    def _handle_PortStatus(self, event):
+    # def _handle_PortStatus(self, event):
+    def _handle_openflow_PortStatus(self, event):
         """ Will be called when a link changes. """
         if event.ofp.desc.config == 1:  # means that link is down
             # event.dpid - switch id
@@ -353,17 +337,54 @@ class Discovery(object):
             pass
         pass
 
-    def _handle_PacketIn(self, event):
+    def _handle_openflow_PacketIn(self, event):
         """ Will be called when a packet is sent to the controller. """
-        if event.parsed.type == pkt.ethernet.LLDP_TYPE:
-            lldp_pkt = event.parsed
-            lldp_p = lldp_pkt.payload
-            ch_id = lldp_p.tlvs[0]
-            po_id = lldp_p.tlvs[1]
-            r_dpid = int(ch_id.id)
-            r_port = int(po_id.id)
+        lldp_pkt = event.parsed
 
-        pass
+        if lldp_pkt.effective_ethertype != pkt.ethernet.LLDP_TYPE or lldp_pkt.dst != pkt.ETHERNET.LLDP_MULTICAST:
+            return EventContinue
+
+        lldp_p = lldp_pkt.payload
+        ch_id = lldp_p.tlvs[0]
+        po_id = lldp_p.tlvs[1]
+        r_dpid = int(ch_id.id)
+        r_port = int(po_id.id)
+
+        log.debug('_handle_openflow_PacketIn-> got LLDP packet: type={}, dpid={}, packet: dpid={}, port={}'
+                  .format(event.parsed.type, event.dpid, r_dpid, r_port))
+
+        if (event.dpid, event.port) == (r_dpid, r_port):
+            log.warning('Port received its own LLDP packet: dpid={}, port={} - ignoring'
+                        .format(event.dpid, event.port))
+            return EventHalt
+
+        # add/update link time
+        link = Discovery.Link(r_dpid, r_port, event.dpid, event.port)
+        if link not in self._adjacency:
+            self._adjacency[link] = time.time()
+            log.info('link detected: {}.{} -> {}.{}'.format(link.dpid1, link.port1, link.dpid2, link.port2))
+        else:
+            # Just update timestamp
+            self._adjacency[link] = time.time()
+
+        # do not pass LLDP packet to switch
+        return EventHalt
+
+    def _delete_links(self, links):
+        for link in links:
+            del self._adjacency[link]
+
+    def _expire_links(self):
+        """ Remove apparently dead links """
+        now = time.time()
+
+        expired = [link for link, timestamp in self._adjacency.iteritems() if timestamp + self._link_timeout < now]
+        if expired:
+            for link in expired:
+                log.debug('_expire_links-> removing link due to timeout: {}.{} -> {}.{}'
+                          .format(link.dpid1, link.port1, link.dpid2, link.port2))
+
+            self._delete_links(expired)
 
 
 def launch():
@@ -375,7 +396,9 @@ def launch():
         Tutorial(event.connection)
 
     core.register('discovery', Discovery())
-    core.openflow.addListenerByName("ConnectionUp", start_switch)
+    # core.registerNew(Discovery)
+    # TODO: removed for debug
+    #core.openflow.addListenerByName("ConnectionUp", start_switch)
 
 
 # log.info('sending to: {0}'.format(str(dstHostInfo)))
